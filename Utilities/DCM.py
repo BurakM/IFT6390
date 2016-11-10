3b074a9e6668c20bc5b04343274105a324e10e51
Mikaël Simard <mikael.simard5@gmail.com>
2016-11-10 08:14:06 -0500

    Example of texture analysis
    - created the subfolder Structures/Dicom which holds dicom images of
    all current patients with masks defining fibrosis/healthy lung/no lung.
    - updated main.py to easily process an example slice of a patient and
    produce its textures.

--- Utilities/DCM.py
+++ Utilities/DCM.py
@@ -1,6 +1,10 @@
-from os import walk, path, fork, getcwd, mkdir # Load files (I'm using os.walk)
-import dicom
+from os import walk, path, fork, getcwd, mkdir, makedirs
+import dicom, dicom.UID
+from dicom.dataset import Dataset, FileDataset
+import datetime, time
 import numpy as np
+from skimage import img_as_ubyte
+from scipy import misc
 
 def load_images_in_folder(folder):
 
@@ -34,10 +38,97 @@ def import_patient(folder):
 	for files in file_list_patient:
 
 		ds_temp = dicom.read_file(files)
-		pat[:,:,ct] = ds_temp.pixel_array
+		pat[:,:,ct] = ds_temp.pixel_array#*ds_temp.slope
 		ct += 1
 
+	# Return the image scaled as a 8 bit integer - required for textures.
+	if np.dtype(ds_temp.pixel_array[0,0]) == 'uint8': # if already a uint, return this.
+		out = pat
+	else:
+		out = misc.bytescale(pat)
 
-	return pat		
+	return out
+
+def write_dicom_8bit(PIX_ARRAY,filename):
+    """
+    INPUTS:
+    pixel_array: 2D numpy ndarray.  If pixel_array is larger than 2D, errors.
+    filename: string name for the output file.
+    """
+
+    file_meta = Dataset()
+    file_meta.MediaStorageSOPClassUID = 'Secondary Capture Image Storage'
+    file_meta.MediaStorageSOPInstanceUID = '999'
+    file_meta.ImplementationClassUID = '999'
+    ds = FileDataset(filename, {},file_meta = file_meta,preamble="\0"*128)
+    ds.Modality = 'CT'
+    ds.ContentDate = str(datetime.date.today()).replace('-','')
+    ds.ContentTime = str(time.time()) #milliseconds since the epoch
+    ds.StudyInstanceUID =  '999'
+    ds.SeriesInstanceUID = '999'
+    ds.SOPInstanceUID =    '999'
+    ds.SOPClassUID = 'Secondary Capture Image Storage'
+    ds.SecondaryCaptureDeviceManufctur = 'Python 2.7.3'
+
+    ## These are the necessary imaging components of the FileDataset object.
+    ds.SamplesPerPixel = 1
+    ds.PhotometricInterpretation = "MONOCHROME2"
+    ds.PixelRepresentation = 0
+    ds.HighBit = 7
+    ds.BitsStored = 8
+    ds.BitsAllocated = 8
+    ds.SmallestImagePixelValue = '\\x00\\x00'
+    ds.LargestImagePixelValue = '\\xff\\xff'
+    ds.Columns = PIX_ARRAY.shape[0]
+    ds.Rows = PIX_ARRAY.shape[1]
+    if PIX_ARRAY.dtype != np.uint8:
+        PIX_ARRAY = PIX_ARRAY.astype(np.uint8)
+    ds.PixelData = PIX_ARRAY #.tostring()
+    ds.save_as(filename)
+    return
+
+def CreatePatientMask(PATIENT_NO,pat_Nsl):
+
+	Npx = 512 # Assume images are all 512x512.
+
+	# Make another test with already preallocated slices.
+	mask_pat = np.zeros((Npx,Npx,pat_Nsl))
+	base_path = getcwd()
+
+	for ii in range(pat_Nsl):
+
+		with open(base_path + '/Structures/Txt/' + PATIENT_NO + '/' + str(ii+1) + '.txt') as f:
+
+			F = f.readlines() # Read each line. the first line will give 1 or 0 (file is empty or not).
+
+			cpt = 0 # Set counter to zero.
+
+			if int(F[0]) >0.5: # if you have something in the slice, then fill it with new values.
+
+				for line in F:
+
+					if cpt>1:
+
+						splitted_line = line.split()
+						mask_pat[:,cpt-2,ii] = [int(i) for i in (splitted_line[0][::2])]
+
+					cpt += 1	
+
+		dicom_file_folder = base_path + '/Structures/Dicom/' + PATIENT_NO + '/' 
+
+		if not path.exists(dicom_file_folder):
+			makedirs(dicom_file_folder)
+
+		# make the number
+		if len(str(ii+1))==1:
+			numero = '00' + str(ii+1)
+		elif len(str(ii+1))==2:
+			numero = '0' + str(ii+1)
+		else:
+			numero = str(ii+1)
+
+		filename_write_dicom = dicom_file_folder + PATIENT_NO + '_mask_' + numero + '.dcm'
+		write_dicom_8bit(mask_pat[:,:,ii],filename_write_dicom)	
 
 
+	return	
--- main.py
+++ main.py
@@ -2,39 +2,102 @@ import dicom
 import matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
-import skimage
+from skimage import feature, data
 import scipy
-from os import walk, path, fork, getcwd, mkdir
-from Utilities import DCM # Import utilities.
+from os import walk, path, fork, getcwd, mkdir, makedirs
+from Utilities import DCM,Maths # Import utilities.
 
-# Create empty lists that will contain the path for all patient images.
+
+# We now have up to 3 patients. Specify mask 'a', 'b' or '' if there are multiple contours.
+PATIENT = 1
+MASK = 'a'
+sl = 173 # Example slice to display and calculate textures for now.
+
+## Create 3D maps of the masks - uncomment if you have not created the dicom files.
+# Example :DCM.CreatePatientMask('P1a',368) # 368
+
+## Create lists with paths for each patients.
 base_path = getcwd() 
-DicomFiles_Pat1 = list()
-DicomFiles_Pat2 = list()
-DicomFiles_Pat3 = list()
+DicomFiles_Pat1 = list() 
+DicomFiles_Pat1_Mask = list()
 
-# I'm using my own local path as we're not gonna upload the images on github. 
+# I'm using my own local path as we're not gonna upload the images on github - everyone should change this to their own local path.
 MikaelPath = "/Users/Mikael/Dropbox/PhD/Classes/IFT6390/Data_Project/" # This is my base path.
 
-DicomFiles_Pat1.append(MikaelPath + "patient1_images_anon")
-DicomFiles_Pat2.append(MikaelPath + "patient2_images_anon")
-DicomFiles_Pat3.append(MikaelPath + "patient3_images_anon")
+DicomFiles_Pat1.append(MikaelPath + "patient" + str(PATIENT) +"_images_anon")# Create full path of patient.
+DicomFiles_Pat1_Mask.append(base_path + '/Structures/Dicom/' + 'P' + str(PATIENT) + MASK)# Create full path of patient mask.
 
-# Load 3D volume for all patients.
+# # Load 3D volume with CT data.
 pat1 = DCM.import_patient(DicomFiles_Pat1)
-pat2 = DCM.import_patient(DicomFiles_Pat2)
-pat3 = DCM.import_patient(DicomFiles_Pat3)
 
-# Display one slice of each patient.
-sl = 250
+# Load 3D volume mask.
+pat1_mask = DCM.import_patient(DicomFiles_Pat1_Mask)
+# Get the mask                : pat1_mask>0
+# Get the fibrosis voxels     : pat1_mask==2
+# Get the non-fibrosis voxels : pat1_mask==1
+
+# Display example slice.
 fig, ax = plt.subplots(2, 2)
 ax[0,0].set_title("Patient 1, slice " + str(sl))
 ax[0,0].imshow(pat1[:,:,sl], cmap=plt.cm.bone)
-ax[0,1].set_title("Patient 2, slice " + str(sl))
-ax[0,1].imshow(pat2[:,:,sl], cmap=plt.cm.bone)
-ax[1,0].set_title("Patient 3, slice " + str(sl))
-ax[1,0].imshow(pat3[:,:,sl], cmap=plt.cm.bone)
+ax[0,1].imshow(pat1_mask[:,:,sl], cmap=plt.cm.bone)
+ax[1,0].imshow(np.multiply(pat1[:,:,sl],pat1_mask[:,:,sl]), cmap=plt.cm.bone)
 plt.show()
 
-# Code continues here
-# and here.
+########################################################################
+# 					TEXTURE ANALYSIS
+########################################################################
+print "Texture analysis..."
+# Calculate the texture of 1 slice.
+# based on http://scikit-image.org/docs/dev/auto_examples/plot_glcm.html
+
+# # Say we test centered at the following locations : should be generalized
+# # to ALL pixels of interest. Also for now fixed at one slice.
+
+lung_patches = [] # This will include a neighborhood of each lung location.
+
+# Define the neighborhood of each location : for now, a square region of size PATCH_SIZE around the voxel.
+PATCH_SIZE = 5 # We could test a circular region or whatever...
+
+# Identify the couples (y,x) in the slice where we have lung :
+AllPerm = np.argwhere(pat1_mask[:,:,sl]>0)
+
+lung_locations = AllPerm.tolist()
+#locations are [(y,x)]. y are the lines in the image, and x are the rows.
+
+for loc in lung_locations:
+ 	lung_patches.append(pat1[loc[0]-PATCH_SIZE:loc[0]+PATCH_SIZE,loc[1]-PATCH_SIZE:loc[1]+PATCH_SIZE,sl])
+
+# Calculate some GLCM properties for each patch.
+diss = []
+corr = []
+k = 0
+for patch in lung_patches:
+ 	# Other choices : 
+ 	# 'contrast','dissimilarity','homogeneity','ASM','energy','correlation'
+ 	glcm = feature.greycomatrix(patch.astype(np.uint8), [5], [0], 256, symmetric=True, normed=True)
+ 	diss.append(feature.greycoprops(glcm,'dissimilarity')[0,0])
+ 	corr.append(feature.greycoprops(glcm,'correlation')[0,0])
+ 	k += 1
+
+dissimilarity_image = np.zeros([512,512])
+correlation_image = np.zeros([512,512])
+
+cpt = 0
+for loc in lung_locations:
+ 	dissimilarity_image[loc[0],loc[1]] = diss[cpt]
+ 	correlation_image[loc[0],loc[1]] = corr[cpt]
+ 	cpt += 1
+
+
+fig, ax = plt.subplots(2, 2)
+ax[0,0].set_title("Dissimilarity")
+ax[0,0].imshow(dissimilarity_image, cmap=plt.cm.bone)
+ax[0,1].set_title("Correlation")
+ax[0,1].imshow(correlation_image, cmap=plt.cm.bone)
+plt.show()
+
+
+
+
+
